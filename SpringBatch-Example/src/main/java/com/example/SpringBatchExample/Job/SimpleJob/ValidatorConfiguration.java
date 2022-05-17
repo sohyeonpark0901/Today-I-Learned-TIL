666ab45dd2024d80a1f5af6bdf6c9a8bf301c0b7
sohyeonpark0901 <shg5865@naver.com>
2022-05-17 17:19:14 +0900

    Add : Batch Validator

---
+++ SpringBatch-Example/src/main/java/com/example/SpringBatchExample/Job/SimpleJob/ValidatorConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.SpringBatchExample.Job.SimpleJob;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ValidatorConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob(){
+        return this.jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .validator(new CustomJobParametersValidator())
+                .build();
+    }
+
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    public Step step2(){
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    public Step step3(){
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step3 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
